ae247c1a7e0fa596c73cc89777fda9ad366607ae
Gyorgy Raffay <Gyorgy_Raffay@epam.com>
2018-09-07 15:17:50 +0200

    Continue5th chapter

--- src/App.js
+++ src/App.js
@@ -5,30 +5,11 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      {name: 'Gunter', age: 22},
-      {name: 'Max', age: 21},
-      {name: 'Agi', age: 25}
-    ]
-  }
-
-  changeNameHandler = (name) => {
-    this.setState({
-      persons: [
-        {name: name, age: 24},
-        {name: 'Maxim', age: 27},
-        {name: 'Agi', age: 25}
-      ]
-    });
-  }
-
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        {name: 'Manu', age: 24},
-        {name: event.target.value, age: 27},
-        {name: 'Agi', age: 25}
-      ]
-    });
+      { id:'as22', name: 'Gunter', age: 22 },
+      { id:'as1112', name: 'Max', age: 21 },
+      { id:'as6546', name: 'Agi', age: 25 }
+    ],
+    isPersonsVisible: false
   }
 
   styleSheet = {
@@ -39,20 +20,71 @@ class App extends Component {
     fontSize: "17px"
   }
 
+
+  togglePersonHandler = () => {
+    const personsVisible = this.state.isPersonsVisible;
+    this.setState({
+      isPersonsVisible: !personsVisible
+    })
+  }
+
+  deletePersonHandler = (person, index) => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(index, 1);
+    this.setState({ persons });
+
+  }
+
+
+  changeNameHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((person) => {
+      return person.id === id
+    });
+
+    const personCopy = {
+      ...this.state.persons[personIndex]
+    }
+
+    //const personCopy = Object.assign({},this.state.persons[personIndex]);
+    personCopy.name = event.target.value;
+
+    const persons = [
+      ...this.state.persons
+    ];
+
+    persons[personIndex] = personCopy;
+    //this.setState({ persons : persons  })
+    this.setState({ persons  });
+    
+  }
+
   render() {
+    let personsContainer = null;
+    if (this.state.isPersonsVisible) {
+      personsContainer =
+        (<div>
+          {
+            this.state.persons.map((person, index) => {
+              return (
+                <Person 
+                name = {person.name}
+                age = {person.age}
+                click = {() => {
+                  this.deletePersonHandler(person, index);
+                }}
+                key = {person.id}
+                changed = {(event) => this.changeNameHandler(event, person.id)}/>
+              )
+            })
+          }
+        </div>)
+    }
     return (
       <div className="App">
-       <h4>React</h4>
-       <button style={this.styleSheet} onClick={this.changeNameHandler.bind(this, 'Ejjj')}>Click</button>
-       <Person age={this.state.persons[0].age} 
-       name={this.state.persons[0].name} />
-       <Person age={this.state.persons[1].age}
-       name={this.state.persons[1].name}
-       changeHandler = {this.changeNameHandler.bind(this, 'Eger')}
-       nameHandler = {this.nameChangedHandler}
-       >Helloka</Person>
-       <Person age={this.state.persons[2].age}
-       name={this.state.persons[2].name}/>
+        <h4>React</h4>
+        <button style={this.styleSheet} onClick={this.togglePersonHandler}>Click</button>
+        {personsContainer}
       </div>
     );
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,9 +4,9 @@ import './Person.css';
 const person = (props) => {    
     return (
       <div className="Person">
-         <h1 onClick={props.changeHandler}>I'm {props.name} and I'm {props.age} years olds</h1>
+         <p onClick={props.click}>I'm {props.name} and I'm {props.age} years olds</p>
          <p>{props.children}</p>
-         <input type="text" onChange={props.nameHandler} value={props.name} />
+         <input type="text" onChange={props.changed} value={props.name} />
       </div>
 
     )
